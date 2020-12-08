75c9b2653fbc354f3b04e877ca566230e86b0ccc
yawar2019 <yawarali17@gmail.com>
2020-12-08 08:03:17 +0530

    committed code

--- MVC4Dec2020Batch/Controllers/NewController.cs
+++ MVC4Dec2020Batch/Controllers/NewController.cs
@@ -95,7 +95,7 @@ namespace MVC4Dec2020Batch.Controllers
             return View(listobj);
         }
 
-        public ViewResult getmeData5()
+        public ViewResult getmeData5(int id)
         {
             List<Student> listobj = new List<Student>();
 
@@ -138,6 +138,87 @@ namespace MVC4Dec2020Batch.Controllers
             sft.ft = listfobj;
 
             return View(sft);
+        }
+
+        public RedirectResult GoToYAhoo() {
+
+            return Redirect("http://www.yahoo.com");
+        }
+        public RedirectResult GoToLocalMethod(int ? id)
+        {
+
+            return Redirect("~/new/getmeData5?id=1");
+        }
+
+        public RedirectToRouteResult gotoRoute()
+        {
+            return RedirectToRoute("xyz");
+
+        }
+
+        public RedirectToRouteResult gotoRoute2()
+        {
+            return RedirectToAction("GoToLocalMethod","new",new { id=1});
+
+        }
+
+        public JsonResult getjsondata()
+        {
+            List<Student> listobj = new List<Student>();
+
+            Student st = new Models.Student();
+            st.studId = 1;
+            st.StudentName = "Krishna";
+            st.Course = "MVC";
+
+            Student st1 = new Models.Student();
+            st1.studId = 2;
+            st1.StudentName = "Sree";
+            st1.Course = "Angular";
+
+
+            Student st2 = new Models.Student();
+            st2.studId = 3;
+            st2.StudentName = "Vidhi";
+            st2.Course = "Phyton";
+
+            listobj.Add(st);
+            listobj.Add(st1);
+            listobj.Add(st2);
+            return Json(listobj,JsonRequestBehavior.AllowGet);
+
+        }
+
+        public FileResult GetFile()
+        {
+            return File("~/Web.config", "text");
+        }
+        public FileResult GetFile1()
+        {
+            return File("~/Web.config", "application/xml");
+        }
+
+        public FileResult GetFile2()
+        {
+            return File("~/Web.config", "application/xml", "Web.config");
+        }
+        public ContentResult GetMyContent(int? id)
+        {
+            if (id == 1) {
+                return Content("hello World");
+            }
+            else if (id == 2) {
+                return Content("<script>alert('hello World')</script>");
+            }
+            else {
+                return Content("<p style=color:red>hello world</p>");
+            }
+           
+        }
+
+        public ActionResult GetPartialView()
+        {
+            return View();
         }
     }
 }
