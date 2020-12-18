19c0055067a23ed330c2373f8235df9ad4d4155a
yawar2019 <yawarali17@gmail.com>
2020-12-18 07:48:09 +0530

    comitted code

---
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstApproach.Models;
+using System.Data.Entity;
+namespace CodeFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);
+            int i=db.SaveChanges();
+            if (i > 0) { return RedirectToAction("Index"); }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State=EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i > 0) { return RedirectToAction("Index"); }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)// var s=1,2,3
+        {
+            
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            db.EmployeeModels.Remove(emp);
+
+            int i = db.SaveChanges();
+            if (i > 0) { return RedirectToAction("Index"); }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ DataBasedApproach/Models/EmployeeDepartment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataBasedApproach.Models
+{
+    public class EmployeeDepartment
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int? EmpSalary { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+
+    }
+}
